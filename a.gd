fb0a6bb853d4595e0731797af469e0760fc3d818
Sandeep <deviator206@gmail.com>
2019-09-27 14:06:34 -0500

    Update a.gd

--- a.gd
+++ a.gd
@@ -1,10 +1,8 @@
 
 Content-Type: application/json
-Authorization: key=AAAAn20fA3Q:APA91bHtl_CtrbWDCErXTg2t5sQI-rQFQoZrLkZWuWV6MoGyCFFFOMm6s77C7PirEaDDZ3xazTXen_VsXWZeU6ZTu9bvJYSCfYsvBXCmD7v5AdQin4gBSvdqplvc2usZtUOvt1f7T_b1
 
 payload 
 {
-  "registration_ids":["c2lnJjywOqM:APA91bHVZMnktCuvYDA2a6VLwReA3eO6aYIvC0S7QUJFA3UewUI2wxEknAGhJjp38nCN1UQ9nJEN8tXHKv4klnm1JYA4KTDCjWNkLVm_B-4mXseObzPawI8zs2aCiCFJGEmnfJQC91eB"],
   "notification" : {
  "body" : "DONt Know WHY!",
  "title" : "Hey buddy",
